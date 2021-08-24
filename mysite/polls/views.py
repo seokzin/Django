d2a7f66fb2cd0ee756cfa1dfe51649bbbc7be828
seokzin <seokzin95@gmail.com>
2021-08-25 02:20:14 +0900

    refac: Remove url paths dependency
    - 하드 코딩된 URL을 path()에 정의된 name으로 교체하여 변수처럼 다룸
    - 또한 namespace를 도입하여 동일한 name에도 앱을 구분지을 수 있음

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Question
@@ -14,10 +15,10 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 
-# def results(request, question_id):
-#     response = "You're looking at the results of question %s."
-#     return HttpResponse(response % question_id)
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
 
 
-# def vote(request, question_id):
-#     return HttpResponse("You're voting on question %s." % question_id)
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
