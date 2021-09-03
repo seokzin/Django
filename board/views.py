4d5003f056081c7c873265e1e2a771f886596382
seokzin <seokzin95@gmail.com>
2021-09-03 11:47:57 +0900

    feat: Add URL routing

--- board/views.py
+++ board/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Question
 
-# Create your views here.
+def new(request):
+    return render(request, )
+
+
+def create(request):
+    pass
+
+
+def index(request):
+    return render(request, )
+
+
+def detail(request, pk):
+    return render(request, )
+
+
+def edit(request, pk):
+    return render(request, )
+
+
+def update(request, pk):
+    pass
+
+
+def delete(request, pk):
+    pass
