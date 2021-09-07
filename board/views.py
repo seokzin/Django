57389f9060ad8481995b989daacec290d10bfda2
seokzin <seokzin95@gmail.com>
2021-09-07 15:04:01 +0900

    feat: Simplify C U views

--- board/views.py
+++ board/views.py
@@ -1,58 +1,73 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_safe, require_POST, require_http_methods
 from .models import Question
+from .forms import QuestionForm
 
-def new(request):
-    return render(request, 'board/new.html')
-
-
+@require_http_methods(['GET', 'POST'])
 def create(request):
-    question = Question()
-    question.title = request.POST.get('title')
-    question.category = request.POST.get('category')
-    question.content = request.POST.get('content')
-    question.save()
+    # 5. 사용자가 데이터를 입력 & POST /board/create/ => invalid data
+    # 10. 사용자가 데이터를 입력 & POST /board/create/ => valid data
+    if request.method == 'POST':
+        # 6. 데이터를 검증할 QuestionForm 인스턴스 초기화 => 내용 있음
+        # 11. 데이터를 검증할 QuestionForm 인스턴스 초기화 => 내용 있음
+        form = QuestionForm(request.POST)
+        # 7. 검증 => 실패
+        # 12. 검증 => 성공
+        if form.is_valid():
+            # 13. form 을 통해 데이터 저장
+            question = form.save()  
+            # 14. /board/<pk>/ 로 redirect 하도록 응답
+            return redirect('questions:detail', question.pk)
+    # 1. GET /board/create/ => 빈 form 요청
+    else:
+        # 2. 비어있는 QuestionForm 인스턴스를 초기화 => 빈 form 생성
+        form = QuestionForm()
 
-    return redirect('board:detail', question.pk)
+    # 3. 빈 form 을 context에 담음
+    # 8. 에러 메시지를 담은 form을 context에 담음
+    context = {'form': form}
+
+    # 4. 사용자에게 빈 form 제공
+    # 9. 에러 미시지를 포함한 form 제공
+    return render(request, 'board/form.html', context)
 
 
+@require_http_methods(['GET', 'SAFE'])
 def index(request):
-    questions = Question.objects.all()
-    context = {
-        'questions': questions,
-    }
+    # pk 내림차순 정렬
+    questions = Question.objects.order_by('-pk')
+    context = {'questions': questions,}
     return render(request, 'board/index.html', context)
 
 
-def detail(request, pk):
-    question = Question.objects.get(pk=pk)
-    context = {
-        'question': question,
-    }
+@require_safe
+def detail(request, question_pk):
+    question = get_object_or_404(Question, pk=question_pk)
+    context = {'question': question,}
     return render(request, 'board/detail.html', context)
 
 
-def edit(request, pk):
-    question = Question.objects.get(pk=pk)
+@require_http_methods(['GET', 'POST'])
+def update(request, question_pk):
+    question = get_object_or_404(Question, pk=question_pk)
+    
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save()              
+            return redirect('questions:detail', question.pk)
+    else:
+        form = QuestionForm(instance=question)
+    
     context = {
         'question': question,
+        'form': form,
     }
-    return render(request, 'board/edit.html', context)
+    return render(request, 'board/form.html', context)
 
 
-def update(request, pk):
-    question = Question.objects.get(pk=pk)
-    question.title = request.POST.get('title')
-    question.category = request.POST.get('category')
-    question.content = request.POST.get('content')
-    question.save()
-
-    return redirect('board:detail', question.pk)
-
-def delete(request, pk):
-    question = Question.objects.get(pk=pk)
-
-    if request.method == 'POST':
-        question.delete()
-        return redirect('board:index')
-    else:
-        return redirect('board:detail', question.pk)
+@require_POST
+def delete(request, question_pk):
+    question = get_object_or_404(Question, pk=question_pk)
+    question.delete()
+    return redirect('questions:index')
