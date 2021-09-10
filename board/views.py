e38874e4e3e8ca5d9c8c1ef8fcd48a6646d52feb
seokzin <seokzin95@gmail.com>
2021-09-10 12:54:35 +0900

    feat: Add paginator

--- board/views.py
+++ board/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_safe, require_POST, require_http_methods
 from .models import Question
@@ -20,7 +21,13 @@ def create(request):
 @require_http_methods(['GET', 'SAFE'])
 def index(request):
     questions = Question.objects.order_by('-pk')
-    context = {'questions': questions,}
+
+    paginator = Paginator(questions, 2)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {'page_obj': page_obj,}
     return render(request, 'board/index.html', context)
 
 
